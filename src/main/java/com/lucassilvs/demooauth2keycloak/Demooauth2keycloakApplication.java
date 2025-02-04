b437895f1cb473ee8ce4869293dfbe559c2bcdc1
Lucas Santos <lucas.parkl@outlook.com>
2025-02-03 21:26:24 -0300

    implementação de Java com autorization code flow - Spring boot com Keycloak:
    - adicionando projeto inicial

---
+++ src/main/java/com/lucassilvs/demooauth2keycloak/Demooauth2keycloakApplication.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.demooauth2keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demooauth2keycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demooauth2keycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucassilvs/demooauth2keycloak/Demooauth2keycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.demooauth2keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demooauth2keycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
